bdf453777270ca5780d06c957e8c0ecbb2208aa1
Deneb331 <juanjo1.seriousmail@gmail.com>
2022-10-26 19:07:57 +0000

    Add validation to all the options used in the survey

--- run.py
+++ run.py
@@ -32,13 +32,13 @@ class Question:
 
 
 question_list = [
-    Question("What is your first name?", "text", None),
-    Question("What is your last name?", "text", None),
+    Question("What is your first name?", "text", []),
+    Question("What is your last name?", "text", []),
     Question("What is your blood type?", "options", ["1. A+", "2. A-", "3. B+", "4. B-", "5. O+", "6. O-", "7. AB+", "8. AB-"]),
-    Question("How healthy do you consider yourself on a scale of 1 to 10?", "num10", None),
+    Question("How healthy do you consider yourself on a scale of 1 to 10?", "options", ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]),
     Question("How often do you get a health checkup?", "options", ["1. Once in 3 months", "2. Once in 6 months", "3. Only when needed", "4. Never get done"]),
-    Question("Do you have any chronic diseases?", "text", None),
-    Question("Do you have any hereditary conditions/diseases?", "text", None),
+    Question("Do you have any chronic diseases?", "y/n", []),
+    Question("Do you have any hereditary conditions/diseases?", "options", ["1. High blood pressure", "2. Diabetes", "3. Hemophilia", "4. Thalassemia", "5. Huntington", "6. Other"]),
     Question("How often do you consume alcohol?", "options", ["1. I don't drink", "2. Only occasionally", "3. Two/three times a week", "4. More than two/three times a week"]),
     Question("How often do you consume cigarettes?", "options", ["1. I don't smoke", "2. Only occasionally", "3. One pack a week", "4. More than one pack a week"]),
     Question("Do you consume other drugs?", "options", ["1. I don't consume other drugs", "2. Only occasionally", "3. Two/three times a week", "4. More than two/three times a week"]),
@@ -54,29 +54,53 @@ def question_trigger(current_question):
     """
     Shows the question to the user and get his input.
     """
-    print(current_question.question + "\n")
+    print(current_question.question)
     if current_question.valid == "text":
-        user_answer = input("\n")
-        if question_validation(user_answer, "text"):
+        user_answer = input("\n\n")
+        if question_validation(user_answer, current_question):
             print("The answer is valid!")
             return True
         else:
-            print("You entered an invalid answer, please use only alphabetic characters. \n")
             return False
     elif current_question.valid == "options":
         option_str = "  ".join(current_question.answers)
-        user_answer = input(option_str + "\n")
+        user_answer = input(option_str + "\n\n")
+        if question_validation(user_answer, current_question):
+            print("The answer is valid!")
+            return True
+        else:
+            return False
+    elif current_question.valid == "y/n":
+        user_answer = input("Please answer YES or NO: \n\n")
+        if question_validation(user_answer, current_question):
+            print("The answer is valid!")
+            return True
+        else:
+            return False
 
 
-def question_validation(answer, valid_cond):
+def question_validation(answer, question_to_validate):
     """
     Validates the answer given by the user. If it is valid, let the program 
     continue. If it's not, make the user answer again.
     """
-    if valid_cond == "text":
+    if question_to_validate.valid == "text":
         if answer.isalpha() is True:
             return True
         else:
+            print("You entered an invalid answer, please use only alphabetic characters. \n")
+            return False
+    elif question_to_validate.valid == "options":
+        if answer.isnumeric() is True and int(answer) <= len(question_to_validate.answers) and int(answer) > 0:
+            return True
+        else:
+            print("Please choose one of the following options using the numeric values assigned. \n")
+            return False
+    elif question_to_validate.valid == "y/n":
+        if answer.lower() == "yes" or answer.lower() == "no":
+            return True
+        else:
+            print("Please answer only YES or NO. \n")
             return False
 
 
