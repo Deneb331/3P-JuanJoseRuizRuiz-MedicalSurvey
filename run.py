26d711de2d3381e992fd3cae6a87e3ca406d09d0
Deneb331 <juanjo1.seriousmail@gmail.com>
2022-11-02 18:57:55 +0000

    Add report feature and initial choice to the user

--- run.py
+++ run.py
@@ -25,29 +25,31 @@ class Question:
     Question class that contains the type of validation used for the specific 
     question and its answers in case it has multiple options.
     """
-    def __init__(self, question, valid, answers, final_answer):
+    def __init__(self, question, valid, answers, final_answer, report_type, report_data):
         self.question = question
         self.valid = valid
         self.answers = answers
         self.final_answer = final_answer
+        self.report_type = report_type
+        self.report_data = report_data
 
 
 question_list = [
-    Question("What is your first name?", "text", [], ""),
-    Question("What is your last name?", "text", [], ""),
-    Question("What is your blood type?", "options", ["1. A+", "2. A-", "3. B+", "4. B-", "5. O+", "6. O-", "7. AB+", "8. AB-"], ""),
-    Question("How healthy do you consider yourself on a scale of 1 to 10?", "options", ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"], ""),
-    Question("How often do you get a health checkup?", "options", ["1. Once in 3 months", "2. Once in 6 months", "3. Only when needed", "4. Never get done"], ""),
-    Question("Do you have any chronic diseases?", "y/n", [], ""),
-    Question("Do you have any hereditary conditions/diseases?", "options", ["1. High blood pressure", "2. Diabetes", "3. Hemophilia", "4. Thalassemia", "5. Huntington", "6. Other"], ""),
-    Question("How often do you consume alcohol?", "options", ["1. I don't drink", "2. Only occasionally", "3. Two/three times a week", "4. More than two/three times a week"], ""),
-    Question("How often do you consume cigarettes?", "options", ["1. I don't smoke", "2. Only occasionally", "3. One pack a week", "4. More than one pack a week"], ""),
-    Question("Do you consume other drugs?", "options", ["1. I don't consume other drugs", "2. Only occasionally", "3. Two/three times a week", "4. More than two/three times a week"], ""),
-    Question("Over the past 2 weeks, how often have you felt nervous, anxious, or on edge?", "options", ["1. Not all", "2. Several days", "3. More days than not", "4. Nearly every day"], ""),
-    Question("Over the past 2 weeks, how often have you felt down, depressed, or hopeless?", "options", ["1. Not all", "2. Several days", "3. More days than not", "4. Nearly every day"], ""),
-    Question("Over the past 2 weeks, how often have you felt little interest or pleasure in doing things?", "options", ["1. Not all", "2. Several days", "3. More days than not", "4. Nearly every day"], ""),
-    Question("How would you describe the condition of your mouth and teeth, including false teeth or dentures?", "options", ["1. Excellent", "2. Good", "3. Average", "4. Poor"], ""),
-    Question("How often do you practice some exercise a week?", "options", ["1. More than 4 days a week", "2. 3-4 days a week", "3. 1-2 days a week", "4. Never"], ""),
+    Question("What is your first name?", "text", [], "", "", ""),
+    Question("What is your last name?", "text", [], "", "", ""),
+    Question("What is your blood type?", "options", ["1. A+", "2. A-", "3. B+", "4. B-", "5. O+", "6. O-", "7. AB+", "8. AB-"], "", "%", ""),
+    Question("How healthy do you consider yourself on a scale of 1 to 10?", "options", ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"], "", "average", ""),
+    Question("How often do you get a health checkup?", "options", ["1. Once in 3 months", "2. Once in 6 months", "3. Only when needed", "4. Never get done"], "", "%", ""),
+    Question("Do you have any chronic diseases?", "options", ["1. Yes", "2. No"], "", "%", ""),
+    Question("Do you have any hereditary conditions/diseases?", "options", ["1. High blood pressure", "2. Diabetes", "3. Hemophilia", "4. Thalassemia", "5. Huntington", "6. Other"], "", "%", ""),
+    Question("How often do you consume alcohol?", "options", ["1. I don't drink", "2. Only occasionally", "3. Two/three times a week", "4. More than two/three times a week"], "", "%", ""),
+    Question("How often do you consume cigarettes?", "options", ["1. I don't smoke", "2. Only occasionally", "3. One pack a week", "4. More than one pack a week"], "", "%", ""),
+    Question("Do you consume other drugs?", "options", ["1. I don't consume other drugs", "2. Only occasionally", "3. Two/three times a week", "4. More than two/three times a week"], "", "%", ""),
+    Question("Over the past 2 weeks, how often have you felt nervous, anxious, or on edge?", "options", ["1. Not all", "2. Several days", "3. More days than not", "4. Nearly every day"], "", "%", ""),
+    Question("Over the past 2 weeks, how often have you felt down, depressed, or hopeless?", "options", ["1. Not all", "2. Several days", "3. More days than not", "4. Nearly every day"], "", "%", ""),
+    Question("Over the past 2 weeks, how often have you felt little interest or pleasure in doing things?", "options", ["1. Not all", "2. Several days", "3. More days than not", "4. Nearly every day"], "", "%", ""),
+    Question("How would you describe the condition of your mouth and teeth, including false teeth or dentures?", "options", ["1. Excellent", "2. Good", "3. Average", "4. Poor"], "", "%", ""),
+    Question("How often do you practice some exercise a week?", "options", ["1. More than 4 days a week", "2. 3-4 days a week", "3. 1-2 days a week", "4. Never"], "", "%", ""),
 ]
 
 
@@ -71,13 +73,6 @@ def question_trigger(current_question):
             return True
         else:
             return False
-    elif current_question.valid == "y/n":
-        user_answer = input("Please answer YES or NO: \n\n")
-        if question_validation(user_answer, current_question):
-            print("The answer is valid!")
-            return True
-        else:
-            return False
 
 
 def question_validation(answer, question_to_validate):
@@ -94,17 +89,10 @@ def question_validation(answer, question_to_validate):
             return False
     elif question_to_validate.valid == "options":
         if answer.isnumeric() is True and int(answer) <= len(question_to_validate.answers) and int(answer) > 0:
-            question_to_validate.final_answer = question_to_validate.answers[int(answer) - 1]
+            question_to_validate.final_answer = int(answer) - 1
             return True
         else:
             print("Please choose one of the following options using the numeric values assigned. \n")
-            return False
-    elif question_to_validate.valid == "y/n":
-        if answer.lower() == "yes" or answer.lower() == "no":
-            question_to_validate.final_answer = answer
-            return True
-        else:
-            print("Please answer only YES or NO. \n")
             return False
 
 
@@ -114,22 +102,73 @@ def update_data_sheet():
     """
     all_answers = []
     for ind in question_list:
-        all_answers.append(ind.final_answer)
+        if ind.valid == "text" or ind.valid == "y/n":
+            all_answers.append(ind.final_answer)
+        else:
+            all_answers.append(ind.answers[int(ind.final_answer)])
     SHEET.worksheet("data").append_row(all_answers)
     print("Data worksheet updated")
-    
+
+
+def generate_report():
+    """
+    Generates a report with all the data stored in the sheet and
+    shows the report to the user.
+    """
+    print("Generating report data...\n")
+    data = SHEET.worksheet("data")
+    rows = []
+    for ind in range(2, data.row_count + 1):
+        row = data.row_values(ind)
+        del row[0]
+        del row[0]
+        rows.append(row)
+    for i in question_list:
+        if i.report_type == "%":
+            data_list = []
+            for j in rows:
+                for k in i.answers:
+                    if k in j:
+                        data_list.append(k)
+            for unit in i.answers:
+                if data_list.count(unit) == 0:
+                    continue
+                percentage = round(data_list.count(unit) * 100 / len(data_list), 2)
+                i.report_data += unit + " " + str(percentage) + "%  "
+            print(i.question + " -> " + i.report_data)
+
+
+def update_report_worksheet(data):
+    """
+    Updates the report worksheet with the data obtained in the generate_report() function.
+    """
+    report_worksheet = SHEET.worksheet("report")
+    report_worksheet.append_row(data)
+    print("The report worksheet has been updated")
+
 
 def main():
     """
     Run all program functions.
     """
-    print("Welcome to our medical survey. Please answer truthfully to the following questions:\n")
-    for ind in question_list:
-        is_valid = False
-        while is_valid is False:
-            is_valid = question_trigger(ind)
-    
-    update_data_sheet()
+    print("Welcome to our medical survey. What would you like to do? \n")
+    while True:
+        print("1. Do the survey   2. See the report \n")
+        choice = input()
+        if int(choice) == 1:
+            print("Welcome to the survey. Please, answer the questions truthfully: \n")
+            for ind in question_list:
+                is_valid = False
+                while is_valid is False:
+                    is_valid = question_trigger(ind)
+            update_data_sheet()
+            return False
+        elif int(choice) == 2:
+            generate_report()
+            #update_report_worksheet(report_data)
+            return False
+        else:
+            print("Please enter a valid option. \n")
 
 
 main()
